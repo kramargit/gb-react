eb031c8da1f153e080f121487dbfaf7c6c1ff7d4
kramargit <89024802+kramargit@users.noreply.github.com>
2022-02-05 17:17:22 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Homework, Message } from './components';
+
+function App() {
+  return (
+    <div className='App'>
+      <Homework>
+
+      </Homework>
+    </ div>
+  );
+}
+
+export default App;
